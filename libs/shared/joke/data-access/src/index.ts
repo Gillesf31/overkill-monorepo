77344c9f9c207b36b1d4f24e5c09c1e91a83b37b
Gilles Ferrand <gilles.ferrand31@gmail.com>
2023-09-05 08:30:04 -0400

    Add PNPM

--- libs/shared/joke/data-access/src/index.ts
+++ libs/shared/joke/data-access/src/index.ts
@@ -1,2 +1,2 @@
-export { JokeService } from './lib/joke.service';
-export { JokeType } from './lib/models/joke.type';
+export {JokeService} from './lib/joke.service';
+export {JokeType} from './lib/models/joke.type';
